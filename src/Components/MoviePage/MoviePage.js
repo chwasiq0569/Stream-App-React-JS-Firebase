ece0eb6217db2501a318cda68189ec12b09e6655
unknown <samsunggrandprimeplus056@gmail.com>
2020-11-15 03:09:08 -0800

    Fixed Some Bugs

--- src/Components/MoviePage/MoviePage.js
+++ src/Components/MoviePage/MoviePage.js
@@ -11,6 +11,7 @@ const MoviePage = ({ location, user }) => {
   const base_url = "https://image.tmdb.org/t/p/original";
   const [movie, setMovie] = useState(null);
   const [trailerUrl, setTrailerUrl] = useState(null);
+
   useEffect(() => {
     setMovie(location?.state?.movie);
     window.scrollTo(0, 0);
@@ -26,6 +27,7 @@ const MoviePage = ({ location, user }) => {
         .catch(console.error);
     }
   }, [movie]);
+
   const opts = {
     height: "450",
     width: "100% ",
@@ -33,43 +35,48 @@ const MoviePage = ({ location, user }) => {
       autoplay: 1,
     },
   };
-  return (
-    <div className={trailerUrl ? "MoviePage__Wrapper_Big" : "MoviePage__Wrapper"}>
-      {
-        !movie ? "Loading" : (<div className="inner__Wrapper">
-        <div className="navbarContainer">
-          <Navbar user={user} />
-        </div>
 
-        <div className="upper__Section">
-          <div className="movie__Info">
-            <div className="leftSide">
-              <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-              {/* <h1>Title</h1> */}
-              <p>{movie?.overview}</p>
-              <div className="Rating__Stars">
-                <img src={RatingStars} alt="RatingStars" />
-                <img src={RatingStars} alt="RatingStars" />
-                <img src={RatingStars} alt="RatingStars" />
-                <img src={RatingStars} alt="RatingStars" />
-                <img src={RatingStars} alt="RatingStars" />
+  return (
+    <div
+      className={trailerUrl ? "MoviePage__Wrapper_Big" : "MoviePage__Wrapper"}
+    >
+      {!movie ? (
+        "Loading"
+      ) : (
+        <div className="inner__Wrapper">
+          <div className="navbarContainer">
+            <Navbar user={user} />
+          </div>
+
+          <div className="upper__Section">
+            <div className="movie__Info">
+              <div className="leftSide">
+                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+                {/* <h1>Title</h1> */}
+                <p>{movie?.overview}</p>
+                <div className="Rating__Stars">
+                  <img src={RatingStars} alt="RatingStars" />
+                  <img src={RatingStars} alt="RatingStars" />
+                  <img src={RatingStars} alt="RatingStars" />
+                  <img src={RatingStars} alt="RatingStars" />
+                  <img src={RatingStars} alt="RatingStars" />
+                </div>
+              </div>
+              <div className="rightSide">
+                <img
+                  src={`${base_url}${movie?.poster_path}`}
+                  alt={"Poster__Img"}
+                />
               </div>
             </div>
-            <div className="rightSide">
-              <img
-                src={`${base_url}${movie?.poster_path}`}
-                alt={"Poster__Img"}
-              />
+          </div>
+          {trailerUrl && (
+            <div className="lower__Section">
+              <Youtube videoId={trailerUrl} opts={opts} />
             </div>
-          </div>
+          )}
         </div>
-        {trailerUrl && (
-          <div className="lower__Section">
-            <Youtube videoId={trailerUrl} opts={opts} />
-          </div>
-        )}
-      </div>)
-      }
+      )}
     </div>
   );
 };
